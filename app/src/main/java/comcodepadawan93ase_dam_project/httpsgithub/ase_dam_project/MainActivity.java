3cee545e53525a2f463bf368bf8ad73851cfdc2e
Kovacs Erik <kovaroberik@gmail.com>
2018-12-03 10:19:09 +0200

    Merge branch 'master' of https://github.com/codepadawan93/ase-dam-project
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/MainActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/MainActivity.java
@@ -1,7 +1,11 @@
 package comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Upon tapping on a menu item, display a list of entities if applicable
+        boolean retval = false;
+        Intent intent = null;
+        switch(item.getItemId()) {
+            case R.id.users_menu_item:
+                intent = new Intent(this, null);
+                retval = true;
+                break;
+            case R.id.questionnaires_menu_item:
+                intent = new Intent(this, QuestionnaireListActivity.class);
+                retval = true;
+                break;
+            case R.id.questions_menu_item:
+                intent = new Intent(this, null);
+                retval = true;
+                break;
+            case R.id.history_menu_item:
+                intent = new Intent(this, null);
+                retval = true;
+                break;
+            case R.id.settings_menu_item:
+                intent = new Intent(this, null);
+                retval = true;
+                break;
+            case R.id.contact_menu_item:
+                intent = new Intent(this, null);
+                retval = true;
+                break;
+            case R.id.about_menu_item:
+                intent = new Intent(this, null);
+                retval = true;
+                break;
+            default:
+                retval = super.onOptionsItemSelected(item);
+        }
+        if(retval)
+            this.startActivity(intent);
+        return retval;
     }
 }
--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/QuestionnaireListActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/QuestionnaireListActivity.java
@@ -2,12 +2,27 @@ package comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class QuestionnaireListActivity extends AppCompatActivity {
+
+    private ListView questionnaireList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire_list);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get existing questionnaires (hardcoded for now...)
+        String[] questionnaires = {"Questionnaire1", "Questionnaire2", "Questionnaire3" };
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, questionnaires);
+
+        // Populate list with existing questionnaires
+        questionnaireList = (ListView)findViewById(R.id.questionnaire_list);
+        questionnaireList.setAdapter(adapter);
     }
 }
