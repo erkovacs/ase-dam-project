7bb3d0c9da386924369dae950899d5225fcb7cdc
Kovacs Erik <kovaroberik@gmail.com>
2018-12-12 01:11:21 +0200

    added m2m relation logic on questionnaire

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/QuestionnaireActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/QuestionnaireActivity.java
@@ -1,18 +1,24 @@
 package comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.Calendar;
 
 public class QuestionnaireActivity extends AppCompatActivity {
     private Spinner questionnaireTypePicker;
+    private ListView questionList;
+    private AlertDialog.Builder builder;
     private EditText dateStart;
     private EditText dateEnd;
 
@@ -23,6 +29,8 @@ public class QuestionnaireActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+
+        final QuestionnaireActivity context = this;
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -82,6 +90,49 @@ public class QuestionnaireActivity extends AppCompatActivity {
                 dateEnd.setText(date);
             }
         };
+
+        // Render the list of questions
+        // Get existing questionnaires (hardcoded for now...)
+        String[] questions = {"Question1", "Question2", "Question3" };
+        ArrayAdapter adapter2 = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, questions);
+
+        // Populate list with existing questionnaires
+        questionList = (ListView)findViewById(R.id.questionnaire_question_list);
+        questionList.setAdapter(adapter2);
+
+        // Allow user to pick questions
+        // setup the alert builder
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose the questions:");
+
+        // add a checkbox list
+        String[] questionLabels = {"Question1", "Question2", "Question3", "Question4", "Question5"};
+        boolean[] checkedItems = {true, false, false, true, false};
+        builder.setMultiChoiceItems(questionLabels, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                // user checked or unchecked a box
+            }
+        });
+
+        // add OK and Cancel buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // user clicked OK
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+
+        questionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // create and show the alert dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
 
