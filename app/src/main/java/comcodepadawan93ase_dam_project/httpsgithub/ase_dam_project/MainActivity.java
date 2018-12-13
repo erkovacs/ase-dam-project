967d5cd5a83c38e92120498d722c450e56b88fc6
afrentescu <adelafrentescu@gmail.com>
2018-12-14 00:23:21 +0300

    Merge branch 'master' of https://github.com/codepadawan93/ase-dam-project
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/MainActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.signin.SignIn;
 
 import comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project.Model.Question;
 
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
