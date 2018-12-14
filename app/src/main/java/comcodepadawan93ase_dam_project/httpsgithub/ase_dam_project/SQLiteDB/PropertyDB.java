285ef14fb3473e38cbf1e7305d195b34f8819570
raluganea <raluganeaa@gmail.com>
2018-12-14 17:35:00 +0200

    Created SQLite DB

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/SQLiteDB/PropertyDB.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/SQLiteDB/PropertyDB.java
@@ -6,12 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PropertyDB extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "Myproperties.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Myproperties.db" ;
+    private static final int DATABASE_VERSION = 1 ;
 
     private SQLiteDatabase db;
 
@@ -22,9 +19,9 @@ public class PropertyDB extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        this.db=db;
+        this.db = db;
 
-        final String SQL_CREATE_PROPERTY_TABLE ="CREATE TABLE, " +
+        final String SQL_CREATE_PROPERTY_TABLE ="CREATE TABLE " +
                 PropertyTable.PROPERTY_TABLE + "(" +
                 PropertyTable.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 PropertyTable.COLUMN_NAME + " VARCHAR(128), " +
@@ -65,12 +62,35 @@ public class PropertyDB extends SQLiteOpenHelper {
     }
 
     //select a record
-    public List<Property> getallProperties(){
-        List<Property> property = new ArrayList<Property>();
+    public Cursor getallProperties(){
+        SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = db.query(PropertyTable.PROPERTY_TABLE,
                 null, null,null,null,null,null);
         return cursor;
     }
 
+    //update record
+    public boolean updateProperty(Property property){
+        //define the new values
+        ContentValues cv = new ContentValues();
+        cv.put(PropertyTable.COLUMN_NAME,property.getProperty_name());
+        cv.put(PropertyTable.COLUMN_VALUE,property.getProperty_value());
+
+        //define the WHERE clause
+        long id = property.getProperty_id();
+        final String whereClause = PropertyTable.COLUMN_ID + " =?";
+        String [] clauseArg = new String[] {String.valueOf(id)};
+
+        //execute update
+        int noRecords = db.update(PropertyTable.PROPERTY_TABLE, cv, whereClause, clauseArg);
+
+        //check the results
+        if(noRecords > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
