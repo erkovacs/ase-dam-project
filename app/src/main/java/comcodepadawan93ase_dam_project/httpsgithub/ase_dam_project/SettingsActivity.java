73d7eea5449fc2778a1ac2f551634d986cc43566
Gherghesan <alexgherghe.13@gmail.com>
2018-12-15 16:19:47 +0200

    Custom Adapter Setttings Demo

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/SettingsActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/SettingsActivity.java
@@ -1,21 +1,50 @@
 package comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    ArrayList<DataModelSettings> dataModels;
+    ListView listView;
+    private static CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_main);
 
-        String[] settings = {"Language", "Sound Effects", "In-Game Music", "Edit Profile", "Privacy", "Log Out"};
-        ListAdapter settingsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, settings);
-        ListView listviewSettings = (ListView) findViewById(R.id.listviewSettings);
-        listviewSettings.setAdapter(settingsAdapter);
+        listView=(ListView)findViewById(R.id.list);
+
+        dataModels= new ArrayList<>();
+
+        dataModels.add(new DataModelSettings("Sound Effects", "Surround", "Can't change Sound Effects"));
+        dataModels.add(new DataModelSettings("In-Game Music", "ON", "Default music"));
+        dataModels.add(new DataModelSettings("Profile", "Student", "This is your student profile"));
+        dataModels.add(new DataModelSettings("Privacy","Terms&Conditions","September 15, 2009"));
+        dataModels.add(new DataModelSettings("Log Out", "SignOut", "Can't log out, you are forever stuck in this game"));
+
+        adapter= new CustomAdapter(dataModels,getApplicationContext());
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                DataModelSettings dataModel= dataModels.get(position);
+            }
+        });
     }
+
 }
+
