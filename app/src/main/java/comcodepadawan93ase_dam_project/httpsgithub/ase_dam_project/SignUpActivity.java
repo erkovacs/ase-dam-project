49a9c10ff50e97b5790e7fb93dfc009bbeced498
afrentescu <adelafrentescu@gmail.com>
2018-12-29 17:26:16 +0300

    schelet google maps act fara cheie momentan

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/SignUpActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/SignUpActivity.java
@@ -128,7 +128,7 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         editor.putString("email", Emails);
         editor.putString("role", chosenRole);
         editor.apply();
-        Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
+       // Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
     }
     public void openLogIn(){
         Intent intent = new Intent(this, SignIn.class);
