d45f15b65544d5edf8fc7f16a276bf198820efc1
Gherghesan <alexgherghe.13@gmail.com>
2019-01-04 22:00:58 +0200

    Added DarkMode in Contact Page - style.xml edited and a new resource file added: attrs.xml

--- app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/ContactActivity.java
+++ app/src/main/java/comcodepadawan93ase_dam_project/httpsgithub/ase_dam_project/ContactActivity.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project.SQLiteDB.Property;
 import comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project.SQLiteDB.PropertyDB;
 import comcodepadawan93ase_dam_project.httpsgithub.ase_dam_project.SQLiteDB.PropertyDataSource;
 
 public class ContactActivity extends AppCompatActivity {
+    //switch button for DarkMode
+    private Switch myswitch;
+
     Button button_json;
     private Button btnSend;
 
@@ -52,17 +58,23 @@ public class ContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //switch button methods for DarkMode
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darktheme);
+        }
+        else setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+
         button_json = findViewById(R.id.button_json);
         button_json.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               clickButton();
-           }
-       });
+            @Override
+            public void onClick(View v) {
+                clickButton();
+            }
+        });
 
         //send button
         btnSend =(Button)findViewById(R.id.btnSend);
@@ -73,7 +85,33 @@ public class ContactActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //switch button methods for DarkMode
+        myswitch=findViewById(R.id.myswitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
     }
+    //restart application with default theme white
+    public void restartApp () {
+        Intent i = new Intent(getApplicationContext(), ContactActivity.class);
+        startActivity(i);
+        finish();
+    }
+
     public void clickButton(){
         Intent intent =  new Intent(this, JSONContactHistoryActivity.class);
         startActivity(intent);
